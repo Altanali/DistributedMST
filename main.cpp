bdd648613ab688ec521222291b81f5879b70db69
altannag <altannagji@utexas.edu>
2024-11-05 12:10:32 -0600

    feat: Progress on creating node children

--- main.cpp
+++ main.cpp
@@ -4,21 +4,70 @@
 #include <netinet/in.h> 
 #include <unistd.h>
 #include <cstring>
+#include <vector>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unordered_map>
 #include <arpa/inet.h>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0); 
-	struct sockaddr_in address = {AF_INET, htons(8080), INADDR_ANY}; 
-	cout << "server fd: " << server_fd << endl;
-	bind(server_fd, (struct sockaddr*)&address, sizeof(address)); 
-	listen(server_fd, 3); 
 
-	int new_socket = accept(server_fd, nullptr, nullptr); 
-	if (new_socket < 0) {
-		 std::cerr << "Connection failed" << std::endl; return -1; 
+	int num_nodes = 100;
+	int address_base = 8080;
+	unordered_map<int, vector<int>> adjacency_list = {
+		{0, {1, 2}}, 
+		{1, {0}},
+		{2, {0}}
+	};
+	vector<int> server_fds(num_nodes);
+	vector<struct sockaddr_in> server_addreses(num_nodes);
+	vector<pid_t> node_pids(num_nodes);
+	string node_executable = "./node.exe";
+
+
+	//Write Information to File
+	fstream fs;
+	fs.open("./node_data.txt", fstream::in | fstream::out | std::fstream::app);
+
+	for(int i = 0; i < num_nodes; ++i) {
+		//Create Process and exec(node.exe)
+		server_fds[i] = socket(AF_INET, SOCK_STREAM, 0);
+		server_addreses[i] = {AF_INET, htons(address_base + i), INADDR_ANY}; 
+		fs << to_string(i) << ", " << to_string(server_fds[i]) << endl;
 	}
-	close(server_fd);
-	close(new_socket);
+
+	fs.close();
+	
+
+	for(int node_i = 0; node_i < num_nodes; ++node_i) {
+		pid_t pid = fork();
+		if(pid == -1) {
+			cout << "Failed to fork process" << endl;
+			exit(-1);
+		}
+		if(pid == 0) {
+			//Exec Node
+			char *args[] = {
+				(char *)node_executable.c_str(), (char *)(to_string(node_i).c_str()), nullptr
+			};
+			if(execvp(args[0], args) == -1) {
+				perror("excecvp failed");
+			}
+		}
+		else {
+			node_pids[node_i] = pid;
+		}
+	}
+	
+	cout << "About to wait for children" << endl;
+	for(pid_t pid : node_pids) {
+		int status;
+		waitpid(pid, &status, 0);
+		if(WIFEXITED(status))
+			cout << "child died with wexit status: " << WEXITSTATUS(status) << endl;
+	}
 }
+
--- node.cpp
+++ node.cpp
@@ -4,42 +4,53 @@
 #include <netinet/in.h> 
 #include <unistd.h>
 #include <cstring>
+#include <string>
+#include <stdlib.h>
 #include <arpa/inet.h>
 
-int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_address = {AF_INET, htons(8080)};
-    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+using namespace std;
 
-    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        std::cerr << "Connection failed" << std::endl;
-        return -1;
-    }
+int main(int argc, char **argv) {
+    //Executable Name, Node_ID
+    int node_id = atoi(argv[1]);
+    cout << "Hi my id is " << node_id << endl;
 
-    std::cout << "Process 1 (Client): Connected to Process 2. Type messages or 'exit' to quit.\n";
+    exit(-1*node_id);
 
-    char buffer[1024];
-    std::string message;
 
-    while (true) {
-        std::cout << "Process 1: ";
-        std::getline(std::cin, message);
+    // int sock = socket(AF_INET, SOCK_STREAM, 0);
+    // struct sockaddr_in server_address = {AF_INET, htons(8080)};
+    // inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
 
-        send(sock, message.c_str(), message.size(), 0);
+    // if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+    //     std::cerr << "Connection failed" << std::endl;
+    //     return -1;
+    // }
 
-        if (message == "exit") {
-            std::cout << "Process 1: 'exit' command sent. Closing connection." << std::endl;
-            break;
-        }
+    // std::cout << "Process 1 (Client): Connected to Process 2. Type messages or 'exit' to quit.\n";
 
-        // Receive response from Process 2
-        memset(buffer, 0, sizeof(buffer));
-        int bytes_read = read(sock, buffer, sizeof(buffer));
-        if (bytes_read <= 0) break;
+    // char buffer[1024];
+    // std::string message;
 
-        std::cout << "Process 1 received: " << buffer << std::endl;
-    }
+    // while (true) {
+    //     std::cout << "Process 1: ";
+    //     std::getline(std::cin, message);
 
-    close(sock);
-    return 0;
+    //     send(sock, message.c_str(), message.size(), 0);
+
+    //     if (message == "exit") {
+    //         std::cout << "Process 1: 'exit' command sent. Closing connection." << std::endl;
+    //         break;
+    //     }
+
+    //     // Receive response from Process 2
+    //     memset(buffer, 0, sizeof(buffer));
+    //     int bytes_read = read(sock, buffer, sizeof(buffer));
+    //     if (bytes_read <= 0) break;
+
+    //     std::cout << "Process 1 received: " << buffer << std::endl;
+    // }
+
+    // close(sock);
+    // return 0;
 }
