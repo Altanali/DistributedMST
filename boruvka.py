04cfd6c3054c848b0051f70f430b92a32511661a
Dorcas Wong <dswong@cs.utexas.edu>
2024-12-09 00:54:58 -0600

    done boruvka, no debugging yet

--- boruvka.py
+++ boruvka.py
@@ -2,10 +2,13 @@ import argparse
 import json
 import threading
 import copy
+import queue
 
 ROOT_LIST = None
 ORIGINAL_GRAPH = None
 CURRENT_GRAPH = None
+
+MST_EDGES = queue.Queue()
 
 def get_args(): 
 	parser = argparse.ArgumentParser()
@@ -32,24 +35,50 @@ def step_1(neighbors, own_id):
         if neighbor.get("weight") < best.get("weight"):
             best = neighbor
     
-    global ROOT_LIST
+    global ROOT_LIST, MST_EDGES
     ROOT_LIST[own_id] = best.get("id")
+    MST_EDGES.put((own_id, best.get("id"), best.get("weight")))
 
 def point_to_root(non_root, roots):
     pointers = [ROOT_LIST[i] for i in non_root]
     return set(pointers).issubset(roots)
 
 def step_2(non_root, roots, id):
-     while (not point_to_root(non_root, roots)):
+    while (not point_to_root(non_root, roots)):
         global ROOT_LIST
         ROOT_LIST[id] = ROOT_LIST[ROOT_LIST[id]]
 
 def step_3(id):
+    global CURRENT_GRAPH
+    CURRENT_GRAPH[id]["name"] = ROOT_LIST[id]
+
+def step_4(id):
+    for node_id, node_data in CURRENT_GRAPH.items():
+        if id == node_data.get("name"):
+            for neighbor in node_data.get("neighbors"):
+                neighbor["id"] = ROOT_LIST[neighbor["id"]]
+                CURRENT_GRAPH[id]["neighbors"].append(neighbor)
+            CURRENT_GRAPH.pop(node_id)
+            
+def step_5(neighbors):
+    neighbors.sort(key=lambda x: x["id"])
+    prev = None
+    best = None
+    for neighbor in neighbors:
+        if neighbor.get("id") == prev:
+            if best["weight"] > neighbor["weight"]:
+                neighbors.remove(best)
+                best = neighbor
+            else:
+                neighbors.remove(neighbor)
+        else:
+            prev = neighbor.get("id")
+            best = neighbor
 
 
 # each step will be internally multithreaded
 def boruvka(graph_data):
-    global ROOT_LIST, ORIGINAL_GRAPH, CURRENT_GRAPH 
+    global ROOT_LIST, ORIGINAL_GRAPH, CURRENT_GRAPH
     ROOT_LIST = list(range(len(graph_data)))
     ORIGINAL_GRAPH = graph_data
     CURRENT_GRAPH = copy.deepcopy(graph_data)
@@ -59,11 +88,12 @@ def boruvka(graph_data):
         threads = []
         for node_id, node_data in CURRENT_GRAPH.items():
             neighbors = node_data.get("neighbors", [])
+            #NOTE TO SELF: IS NODE_ID A STR OR INT, WILL THIS AFFECT ANYTHING?
             thread = threading.Thread(target=step_1, args=(neighbors, node_id))
             threads.append(thread)
             thread.start()
 
-        #wait for threads to finish
+        #wait for threads to finish at each step
 
         # THE FOLLOWING IS NOT LISTED IN THE PARALLEL ALGO IN PAPER
         for i in range(len(ROOT_LIST)):
@@ -74,9 +104,28 @@ def boruvka(graph_data):
         non_roots = set(list(range(len(ROOT_LIST)))) - roots
 
         # Step 2: find new root
-        step_2(non_roots, roots, id)
+        for id in non_roots:    
+            # TODO: multithread this
+            step_2(non_roots, roots, id)
 
         # Step 3: 
+        for non_root in non_roots:
+            # TODO: check if types work
+            # TODO: multithread this
+            step_3(non_root)
+
+        # Step 4:
+        for root in roots:
+            # only roots execute so only they append to their own edgelists
+            # TODO: multithread this
+            step_4(root)
+
+        # Step 5:
+        for node_id, node_data in CURRENT_GRAPH.items():
+            # TODO: multithread this
+            neighbors = node_data.get("neighbors", [])
+            step_5(neighbors)
+        
 
 def spawn_threads(graph_data):
 
@@ -90,3 +139,4 @@ def spawn_threads(graph_data):
 if __name__ == "__main__":
     file_path = get_args().file 
     json_data = get_json_data(file_path)
+    boruvka(json_data)
